e54cfbc08069268af4b80587fdcaa3c1ac28dc55
wouter856 <wouterwaumans@live.be>
2023-12-08 11:42:19 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+export default class App {
+    constructor() {
+        console.log('Constructed!');
+        this.getLocation();
+    }
+
+    getLocation() {
+        navigator.geolocation.getCurrentPosition(
+            this.showPosition.bind(this),
+            this.showError()
+        );
+    }
+
+    showPosition(position) {
+        console.log(position);
+        let x = position.coords.latitude;
+        let y = position.coords.longitude;
+        this.getWeather(x, y);
+    }
+
+    getWeather(x, y) {
+        //api key url to ask for all data needed with x and y coordinates
+        fetch(`https://api.open-meteo.com/v1/forecast?latitude=${x}&longitude=${y}&hourly=temperature_2m&current_weather=true&forecast_days=1`)
+            .then(response => response.json())
+            .then(data => {
+                document.querySelector("h2").innerHTML = data.current_weather.temperature + "°C";
+            })
+            .catch(error => console.log(error));
+    }
+
+    showError(error) {
+        console.log(error);
+    }
+}   
